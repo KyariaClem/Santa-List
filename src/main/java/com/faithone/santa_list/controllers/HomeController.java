227a5377eda5c366d066d4815cd85c7f3eab6648
kyariaclemons <kyarialove@gmail.com>
2024-11-26 15:39:57 -0600

    added method for homecontroller, with respons body to return a string welcome message, added ann. notes

--- src/main/java/com/faithone/santa_list/controllers/HomeController.java
+++ src/main/java/com/faithone/santa_list/controllers/HomeController.java
@@ -1,4 +1,23 @@
 package com.faithone.santa_list.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+
+    // Have to make methods for controller now
+    //@Responsebody if returnign a string on page/ do not need @Reponsebody if not returning a string(returning a template)
+
+
+    // @GGetMapping for get request to display data on page ("/) -the tag
+    @GetMapping("/")
+    @ResponseBody
+    public String returnWelcomePage(){
+        return "welcome to santa's homepage";
+    }
+
 }
